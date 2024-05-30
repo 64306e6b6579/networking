1c48d505051147f7b90d684b4dfbd4c5e2f1b95f
64306e6b6579 <141281753+64306e6b6579@users.noreply.github.com>
2024-05-30 15:37:40 -0400

    Add files via upload

--- submask_conversion.py
+++ submask_conversion.py
@@ -1,27 +1,49 @@
 """
-Asks enter a subnet mask in 255.255.255.0 or 255.255.224.0 and
+Asks enter a subnet mask in 255.255.255.0 or 255.255.224.0, and
 it converts to /something
 """
 
-# first take an input of subnet mask make a list out of it and convert to integers
+# first take an input of ip make a list out of it and convert to integers
 
 subnet = input('enter a subnet: ').split('.')
 
+# function that turns the list objects to integers then runs a check and
+# to what class this subnet-mask belongs
 
-def check_subnet_class():
-    subnetmask = [eval(i) for i in subnet]
-    if -1 < subnetmask[0] < 127:
+
+def check_subnet_class(s):
+    ip = [int(item) for item in subnet]
+    if -1 < ip[0] < 127:
         return 'it is A class'
-    elif 127 < subnetmask[0] < 192:
+    elif 127 < ip[0] < 192:
         return 'it is B class'
-    elif 191 < subnetmask[0] < 224:
+    elif 191 < ip[0] < 224:
         return 'it is C class'
-    elif 223 < subnetmask[0] < 240:
+    elif 223 < ip[0] < 240:
         return 'it is D class'
-    elif 239 < subnetmask[0] < 255:
+    elif 239 < ip[0] < 255:
         return 'it is E class'
     else:
         return 'incorrect Entry'
 
 
-print(check_subnet_class())
+# function that takes and subnet dotted notation and returns CIDR notation
+
+def convert_to_slash_notation(s):
+    subnetmask = [int(item) for item in s]
+    subnetmask = [bin(item) for item in subnetmask]
+    subnetmask = [s[2:] for s in subnetmask]
+    count_ones = 0
+    for i in subnetmask:
+        i.split()
+        for b in i:
+            if b == '1':
+                count_ones += 1
+            else:
+                pass
+    subnet_slash_notation = f'This subnet mask is /{count_ones} bits'
+    return subnet_slash_notation
+
+
+print(convert_to_slash_notation(subnet))
+print(check_subnet_class(subnet))
